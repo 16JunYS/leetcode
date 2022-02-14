a3e952aa759159db677878bf34674883702e711b
전연선 <37628205+16JunYS@users.noreply.github.com>
2022-02-14 22:34:41 +0900

    Time: 3 ms (23.88%), Space: 64.2 MB (33.15%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,16 +1,21 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int len = nums.length;
-        int[] tmp = new int[len];
-        for (int i=0; i < len; i++) {
-            tmp[i] = nums[i];
-        }
         int i=0;
-        int idx = len - k%len;
+        int idx = len - k % len;
+        int[] tmp = Arrays.copyOf(nums, idx);
+        boolean isT = false;
         while(i < len) {
-            if (idx == len)
+            if (idx == len) {
                 idx = 0;
-            nums[i++] = tmp[idx++];
+                isT = true;
+            }
+            if (isT) {
+                nums[i++] = tmp[idx++];
+            }
+            else {
+                nums[i++] = nums[idx++];
+            }
         }
     }
 }
